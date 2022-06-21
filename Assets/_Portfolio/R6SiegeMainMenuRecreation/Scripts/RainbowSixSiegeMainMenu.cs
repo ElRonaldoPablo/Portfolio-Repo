a1c4383ed95e2386d4d7e09d859f14a702959c61
Ronaldo Pablo <sentient.realms@gmail.com>
2022-06-20 20:30:33 -0400

    Playlist Button OnPointerEnter/Exit Animations
    Setup of animations for when the pointer exters/exits the Playlist button.

--- Assets/_Portfolio/R6SiegeMainMenuRecreation/Scripts/RainbowSixSiegeMainMenu.cs
+++ Assets/_Portfolio/R6SiegeMainMenuRecreation/Scripts/RainbowSixSiegeMainMenu.cs
@@ -7,6 +7,7 @@ using DG.Tweening;
 
 public class RainbowSixSiegeMainMenu : MonoBehaviour
 {
+    [Header("Splash")]
     [SerializeField] private GameObject _splashPanel;
     [SerializeField] private Image _splashBackgroundImage;
     [SerializeField] private Image _splashFadeImage;
@@ -15,11 +16,20 @@ public class RainbowSixSiegeMainMenu : MonoBehaviour
 
     private Sequence _completeSplashSequence;
 
+    [Header("Playlist")]
+    [SerializeField] private GameObject _playlistButton;
+    [SerializeField] private Image _playlistButtonHighlight;
+    [SerializeField] private Image _playlistButtonFill;
+    [SerializeField] private Image _playlistButtonIcon;
+
+    private Sequence _completePlaylistHighlightSequence;
+    private Sequence _completePlaylistUnhighlightSequence;
+
     // Start is called before the first frame update
     private void Start()
     {
-        InitializeRainbowMiniProject();
-        PlaySplashSequence();
+        //InitializeSplashSequence();
+        //PlaySplashSequence();
     }
 
     // Update is called once per frame
@@ -28,7 +38,9 @@ public class RainbowSixSiegeMainMenu : MonoBehaviour
         
     }
 
-    private void InitializeRainbowMiniProject()
+    #region Splash
+
+    private void InitializeSplashSequence()
     {
         _splashBackgroundImage.color = new Color(0.0f, 0.0f, 0.0f, 1.0f);
         _splashFadeImage.color = new Color(1.0f, 1.0f, 1.0f, 0.0f);
@@ -80,4 +92,91 @@ public class RainbowSixSiegeMainMenu : MonoBehaviour
 
         return _completeSplashSequence;
     }
+
+    #endregion
+
+    #region Playlist Button
+
+    private void InitializePlaylistButton()
+    {
+        _playlistButtonHighlight.color = new Color(1.0f, 1.0f, 1.0f, 0.0f);
+        _playlistButtonHighlight.transform.localScale = new Vector3(1.15f, 1.15f, 1.15f);
+        _playlistButtonFill.transform.localScale = Vector3.one;
+        _playlistButtonIcon.transform.localScale = Vector3.one;
+        _playlistButtonHighlight.gameObject.SetActive(false);
+    }
+
+    public void PlayPlaylistButtonHighlightSequence()
+    {
+        PlaylistButtonHighlightSequence().Play();
+    }
+
+    public void PlayPlaylistButtonUnhighlightSequence()
+    {
+        PlaylistButtonUnhighlightSequence().Play();
+    }
+
+    private Sequence PlaylistButtonHighlightSequence()
+    {
+        Sequence highlight = DOTween.Sequence();
+        highlight
+            .AppendCallback(()=> _playlistButtonHighlight.gameObject.SetActive(true))
+            .Append(_playlistButtonHighlight.rectTransform.DOScale(1.0f, 0.2f))
+            .Join(_playlistButtonHighlight.DOFade(1.0f, 0.2f))
+            .Pause();
+
+        Sequence lightSweep = DOTween.Sequence();
+        lightSweep
+            .AppendCallback(null)
+            .Append(null)
+            .Join(null)
+            .Pause();
+
+        Sequence fill = DOTween.Sequence();
+        fill
+            .Append(_playlistButtonFill.rectTransform.DOScaleX(13.45f, 0.2f))
+            .Pause();
+
+        Sequence icon = DOTween.Sequence();
+        icon
+            .Append(_playlistButtonIcon.rectTransform.DOScale(1.15f, 0.05f))
+            .Append(_playlistButtonIcon.rectTransform.DOScale(1.0f, 0.05f))
+            .Pause();
+
+        _completePlaylistHighlightSequence = DOTween.Sequence();
+        _completePlaylistHighlightSequence
+            .AppendCallback(InitializePlaylistButton)
+            .Append(highlight)
+            .Join(lightSweep) // TO-DO
+            .Join(fill)
+            .Join(icon)
+            .Pause();
+
+        return _completePlaylistHighlightSequence;
+    }
+
+    private Sequence PlaylistButtonUnhighlightSequence()
+    {
+        Sequence unhighlight = DOTween.Sequence();
+        unhighlight
+            .Append(_playlistButtonHighlight.rectTransform.DOScale(1.15f, 0.2f))
+            .Join(_playlistButtonHighlight.DOFade(0.0f, 0.2f))
+            .Pause();
+
+        Sequence fill = DOTween.Sequence();
+        fill
+            .Append(_playlistButtonFill.rectTransform.DOScaleX(1.0f, 0.2f))
+            .Pause();
+
+        _completePlaylistUnhighlightSequence = DOTween.Sequence();
+        _completePlaylistUnhighlightSequence
+            .Append(unhighlight)
+            .Join(fill)
+            .OnComplete(()=> _playlistButtonHighlight.gameObject.SetActive(false))
+            .Pause();
+
+        return _completePlaylistUnhighlightSequence;
+    }
+
+    #endregion
 }
